feb6a1982aee24c12c54b93b989a7c8894b39c97
Thomas-Rutter <thomasgeorgerutter@hotmail.co.uk>
2021-03-31 08:55:27 +0100

    Fixing linting issue in post_gen_project.py hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,21 @@ import subprocess
 
 
 if "{{cookiecutter.setup_repo}}" == "y":
-    subprocess.call(['git', 'init'])
-    subprocess.call(['git', 'add', '*'])
-    subprocess.call(['git', 'commit', '-m', 'Initial Commit'])
-    subprocess.call(['gh', 'repo', 'create', '{{cookiecutter.project_id}}', '-y', '-d', '{{cookiecutter.description}}', '--private'])
-    subprocess.call(['git', 'push', '-u', 'origin', 'master'])
-    subprocess.call(['git', 'checkout', '-b', 'develop'])
-    subprocess.call(['git', 'push', '-u', 'origin', 'develop'])
+    subprocess.call(["git", "init"])
+    subprocess.call(["git", "add", "*"])
+    subprocess.call(["git", "commit", "-m", "Initial Commit"])
+    subprocess.call(
+        [
+            "gh",
+            "repo",
+            "create",
+            "{{cookiecutter.project_id}}",
+            "-y",
+            "-d",
+            "{{cookiecutter.description}}",
+            "--private"
+        ]
+    )
+    subprocess.call(["git", "push", "-u", "origin", "master"])
+    subprocess.call(["git", "checkout", "-b", "develop"])
+    subprocess.call(["git", "push", "-u", "origin", "develop"])
